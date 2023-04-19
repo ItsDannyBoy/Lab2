ea806f938e34af3664014733079a7d535e3a84d5
danih <danih@DESKTOP-GCQI5FM>
2023-04-19 12:53:16 +0300

    Updated file from Niv

--- src/pulse_generator.vhd
+++ src/pulse_generator.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 entity pulse_generator is
 generic (
-	G_CLOCKS_PER_PULSE      : in 	integer := 0  
+	G_CLOCKS_PER_PULSE      : in 	integer := 8  
 );
 port ( 
 	
@@ -14,15 +14,15 @@ port (
 end entity;
 
 architecture behave of pulse_generator is
---************* works but with many flip flops**************
---	signal CLOCK_CNT	: integer := 0;
---	constant G_CLOCKS_PER_PULSE_DOUBLED	: integer := G_CLOCKS_PER_PULSE * 2;
---**********************************************************
---************* works with ONE! flip flops******************
-	subtype integer_range is integer range 0 to G_CLOCKS_PER_PULSE * 2; -- used for limitng integer value of clock_counter and g_clocks_per_pulse_doubled integers
+--***** works but with many flip flops******
+	signal CLOCK_CNT	: integer range 0 to G_CLOCKS_PER_PULSE * 2-1 := 0;
+	constant G_CLOCKS_PER_PULSE_DOUBLED	: integer := G_CLOCKS_PER_PULSE * 2;
+--********************
+--***** works with ONE! flip flops********
+--	subtype integer_range is integer range 0 to G_CLOCKS_PER_PULSE * 2; -- used for limitng integer value of clock_counter and g_clocks_per_pulse_doubled integers
 																		-- helps minimizing the number of flipflops
-	constant G_CLOCKS_PER_PULSE_DOUBLED	: integer_range := G_CLOCKS_PER_PULSE * 2;
-    signal CLOCK_CNT : integer_range := 0;
+	--constant G_CLOCKS_PER_PULSE_DOUBLED	: integer_range := G_CLOCKS_PER_PULSE * 2;
+  --  signal CLOCK_CNT : integer_range := 0;
 
 begin
 
@@ -30,11 +30,12 @@ begin
 	begin
 		if RST = '0' then
 			PULSE <= '0';
+			CLOCK_CNT <= 0;		-- clear the clock counter
 		elsif rising_edge(CLK) then
 			if RATE = '0' then
 				if CLOCK_CNT = G_CLOCKS_PER_PULSE - 1 then
 				CLOCK_CNT <= 0;					-- clear the clock counter
-				PULSE <= '1';						-- generate pulse
+				PULSE <= '1';					-- generate pulse
 				else
 					CLOCK_CNT <= CLOCK_CNT + 1;	-- increment the clock counter
 					PULSE <= '0';
